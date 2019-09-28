86c531fc9c98be03189593c0338c21e221adee95
Nathenael Dereb <derebdvlps@gmail.com>
2019-09-28 14:34:08 -0400

    Monogo implemented

--- hack/src/core.py
+++ hack/src/core.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
-import dns
 
-client = MongoClient("mongodb+srv://Nathan:Activeatlas3@hackumbc-nathenael-uhvrd.gcp.mongodb.net/test?retryWrites=true&w=majority")
-db = client.test
+def connect_monogo():
+    client = MongoClient("mongodb+srv://Nathan:Activeatlas3@hackumbc-nathenael-uhvrd.gcp.mongodb.net/test?retryWrites=true&w=majority")
+    database = client.get_database('HackUMBC19')
+    collection = database['Predection']
+    print(collection.find_one({}))
+    return database
 
-
+connect_monogo()
